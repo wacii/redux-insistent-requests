f3d5459c2f4e43cdc8793e6c5a4b107f83ffee1a
William Cunningham <w.a.cunningham.ii@gmail.com>
2017-08-02 16:20:09 -0400

    Initialize online state when network monitor attached

--- src/networkMonitor.js
+++ src/networkMonitor.js
@@ -3,6 +3,12 @@ import { online, offline } from "./actions";
 function attachMonitor(dispatch) {
   addEventListener("online", () => dispatch(online()));
   addEventListener("offline", () => dispatch(offline()));
+
+  if (navigator.onLine) {
+    dispatch(online());
+  } else {
+    dispatch(offline());
+  }
 }
 
 export default attachMonitor;
